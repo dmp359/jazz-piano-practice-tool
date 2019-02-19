4bb96474db6da01832cd6dc710486a1a9971c2cc
Daniel Perlman <dmp359@drexel.edu>
2019-02-19 11:41:36 -0500

    added username to filename

--- src/jazz-tool/run.py
+++ src/jazz-tool/run.py
@@ -57,38 +57,27 @@ def index():
 # http://flask.pocoo.org/docs/1.0/patterns/fileuploads/
 @app.route("/sheets", methods=["POST"])
 def upload_file():
-    print('This is error output', file=sys.stderr)
-
-	# A
     if "user_file" not in request.files:
-        print('No user_file key in request.files', file=sys.stderr)
         return "No user_file key in request.files"
 
-	# B
     file = request.files["user_file"]
-    print(file.filename, file=sys.stderr)
-
     """
         These attributes are also available
-
         file.filename               # The actual name of the file
         file.content_type
         file.content_length
         file.mimetype
-
     """
 
-	# C.
     if file.filename == "":
         return "Please select a file"
 
-	# D.
+    # TODO: Pull from db
+    user_name = 'john123'
     if file and allowed_file(file.filename):
-        file.filename = secure_filename(file.filename)
-        
-        output   	  = upload_file_to_s3(file, app.config["S3_BUCKET"])
+        file.filename = '{}/{}'.format(user_name, secure_filename(file.filename))
+        output = upload_file_to_s3(file, app.config["S3_BUCKET"])
         return redirect("/sheets")
-
     else:
         return redirect("/sheets")
 
@@ -96,10 +85,6 @@ def upload_file():
 @app.route('/resources/<path:path>')
 def base_static(path):
     return send_file(os.path.join(app.root_path, '..', 'resources', path))
-
-# @app.route('/bikes')
-# def bikes():
-#     return render_template('bikes.html', data=bike_data, base_bikes_url="/img/bikes/")
 
 # Handle any unhandled filename by loading its template.
 @app.route('/<name>')
